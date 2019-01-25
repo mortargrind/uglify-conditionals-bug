0327fa2fcdafd40935847644c10250290574e034
Semih Basmaci <mortargrind@gmail.com>
2019-01-25 12:03:01 +0100

    Code cleanup:
    -Random input generation is removed since it does not affect anything.
    -Logging statements are updated to not use console.error when it's success case.

--- src/test.js
+++ src/test.js
@@ -1,12 +1,7 @@
 const objectAssignment = require('./specs/objectAssignment.js');
 const uglyObjectAssignment = require('../dist/objectAssignment.min.js');
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-const input = getRandomInt(100);
-console.log('input is :', input);
+const input = 5;
 
 const normalContext = {
   objectAssignment,
@@ -22,8 +17,8 @@ const normalResult = normalContext.objectAssignment(input);
 const uglyResult = uglifyContext.uglyObjectAssignment(input);
 
 if (JSON.stringify(normalResult) === JSON.stringify(uglyResult)) {
-  console.error('success for object');
+  console.log('Success!');
 } else {
-  console.error('error for object');
+  console.error('Error!');
   process.exit(-1);
 }
